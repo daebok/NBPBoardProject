5cd18624e5aba7d825fa0d3a6ca4c8bc27d98e39
hyunhye-shin <hyunhye.shin@navercorp.com>
2017-08-16 12:18:30 +0900

    #57585 jsp 공통 에러 페이지 처리
    1. web.xml에서 필터를 적용하여 페이지 에러 별로 처리
    http://iscr01.navercorp.com/issues/57585

--- src/main/java/com/hyunhye/user/model/CommonErrorCode.java
+++ src/main/java/com/hyunhye/user/model/CommonErrorCode.java
@@ -2,12 +2,13 @@ package com.hyunhye.user.model;
 
 public enum CommonErrorCode {
 
-	INVALID_LOGIN_PASSWORD("InvalidLoginPassword", "아이디 또는 비밀번호가 올바르지 않습니다." ),
-	INVALID_CAPTCHA("InvalidCaptcha","자동가입방지문자의 입력이 올바르지 않습니다."),
-	INVALID_LOGIN_USER("InvalidLoginUser","아이디 또는 비밀번호가 올바르지 않습니다.");
-	
+	INVALID_LOGIN_PASSWORD("InvalidLoginPassword", "아이디 또는 비밀번호가 올바르지 않습니다."),
+	INVALID_CAPTCHA("InvalidCaptcha", "자동가입방지문자의 입력이 올바르지 않습니다."),
+	INVALID_LOGIN_USER("InvalidLoginUser", "아이디 또는 비밀번호가 올바르지 않습니다.");
+
 	private String code;
 	private String errorText;
+
 	private CommonErrorCode(String code, String errorText) {
 		this.code = code;
 		this.errorText = errorText;
@@ -16,6 +17,7 @@ public enum CommonErrorCode {
 	public String getErrorText() {
 		return errorText;
 	}
+
 	public static String getErrorText(String code) {
 		CommonErrorCode[] values = CommonErrorCode.values();
 
@@ -26,10 +28,9 @@ public enum CommonErrorCode {
 		}
 		return null;
 	}
+
 	public String getCode() {
 		return code;
 	}
-	
-	
-	
+
 }
